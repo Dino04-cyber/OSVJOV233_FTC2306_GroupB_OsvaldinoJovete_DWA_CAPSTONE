c50a71e1c311cf9df158ca2531dcd0c471dcbe59
Dino04-cyber <djovety@gmail.com>
2023-11-27 16:02:09 +0200

    attempting to display user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,16 @@ import LoginPage from './LoginPage'
 
 
 export default function Navbar() {
+
+  // const user = session.user
+  // console.log(user)
   return (
     <div className="navbar">
       <div className="logo-container">
         <img src="src\Images\logo.png" alt="Podcast Logo" className="logo" />
       </div>
       <div>
-      {<span>Welcome,{session.user} </span>} 
+      <span>Welcome</span>
       </div>
       <div className="login-container">
         <img src="src\Images\login.png" alt="Login" className="login-icon" />
